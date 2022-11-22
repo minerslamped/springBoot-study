f3f59c795858973fa7b92eea6c71184003424cb3
LiuWd <261751065@qq.com>
2022-11-22 10:38:42 +0800

    修改配置

--- springboot-restful/src/main/java/com/pancm/RestfulApp.java
+++ springboot-restful/src/main/java/com/pancm/RestfulApp.java
@@ -5,12 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
-public class App 
+public class RestfulApp
 {
     public static void main( String[] args )
     {
     	// 启动嵌入式的 Tomcat 并初始化 Spring 环境及其各 Spring 组件
-		SpringApplication.run(App.class, args);
+		SpringApplication.run(RestfulApp.class, args);
 		System.out.println("程序正在运行...");
     }
 }
